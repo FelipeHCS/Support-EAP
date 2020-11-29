318a2ede31a8633684709831886eded7469d86d0
unknown <hilariofelipe616@gmail.com>
2020-11-29 13:08:45 -0300

    s

--- commands/general/planos.js
+++ commands/general/planos.js
@@ -9,7 +9,7 @@ const execute = async (bot, message, args) => {
         .addField(':flag_br: :flag_us: | Bot EAP Basic','Ban, Kick, Clear e Ticket => R$ 60.00 \n Link de pagamento: https://mpago.la/2kQGfgH')
         .addField(':flag_br: :flag_us: | Bot EAP Medium','Ban, Kick, Clear, Ticket, Anúncio e Sortear => R$95.00 \n Link de pagamento: https://mpago.la/28sQ4fQ ')
         .addField(':flag_br: :flag_us: | Bot EAP Advanced','Ban, Kick, Clear, Ticket, Anúncio, Sortear, Votação e Música(V1.0) => R$ 120.00 \n Link de pagamento: https://mpago.la/2HQaVNw')
-        .addField(':flag_br :flag_us:: | Bot EAP Magic','Ban, Kick, Clear, Ticket, Anúncio, Sortear, Votação, Música(V1.0), Anti-flood, Anti-invite => R$ 160.00 \n Link de pagamento: https://mpago.la/2Z8m7Bo')
+        .addField(':flag_br: :flag_us:: | Bot EAP Magic','Ban, Kick, Clear, Ticket, Anúncio, Sortear, Votação, Música(V1.0), Anti-flood, Anti-invite => R$ 160.00 \n Link de pagamento: https://mpago.la/2Z8m7Bo')
         .addField(':flag_br: :flag_us: | Bot EAP Music v1.0','Comandos de músicas => R$ 100.00 \n Link de pagamento: https://mpago.la/1qq7u5V')
         .addField(':flag_br: :flag_us: | Bot EAP Music v2.0','Comandos de músicas => R$ 150.00 \n Link de pagamento: https://mpago.la/2apUkjR')
         .addField(':flag_br: | Criação de Servidores (Games) \n :flag_us: | Creation of servers (Games)  ','R$ 85.00 \n link de pagamento: https://mpago.la/1YyXmRq')
