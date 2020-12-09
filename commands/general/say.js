b1862e14763b1332af3552042b39282de506d336
unknown <hilariofelipe616@gmail.com>
2020-12-09 00:15:13 -0300

    ds

--- commands/general/say.js
+++ commands/general/say.js
@@ -1,30 +1,42 @@
+const Discord = require("discord.js")
+const execute = async (bot, message, args) => {
+    message.delete();
+
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Você não tem permissão para utilizar este comando").then(m => m.delete(5000));
+
+    let per1 = "Em qual canal será enviada a resposta? (Mencione com #)";
+    let per2 = "Qual o conteúdo da mensagem";
+    let per3 = "Qual o título da mensagem?";
+
+    let canal;
+    message.channel.send(per1);
+    let p1 = message.channel.createMessageCollector(m => m.author.id === message.author.id, {
+        max: 1,
+        time: 60000
+    })
+
+    p1.on("collect", async (msg) => {
+        canal = msg.mentions.channels.first()
+        message.channel.send(per2);
+        let p2 = message.channel.createMessageCollector(m => m.author.id === message.author.id, {
+            max: 1,
+            time: 60000
+        });
+
+        p2.on("collect", async () => {
+            message.channel.send(per3);
+            let p3 = message.channel.createMessageCollector(m => m.author.id === message.author.id, {
+                max: 1,
+                time: 60000
+            });
+            p3.on("collect", async () => {
+                let resp = p2.collected.first().content
+                let piru = await message.guild.channels.cache.get(canal.id).send(embed)
+            })
+        })
+    })
+}
 module.exports = {
-    run: (client, message, args) => {
-      if (!message.member.hasPermission(['MANAGE_MESSAGES', 'ADMINISTRATOR'])) { return message.channel.send('> Você não pode usar esse comando!') }
-  
-      let argsresult
-      const mChannel = message.mentions.channels.first()
-  
-      message.delete()
-      if (mChannel) {
-        argsresult = args.slice(1).join(' ')
-        mChannel.send(argsresult)
-      } else {
-        argsresult = args.join(' ')
-        message.channel.send(argsresult)
-      }
-    },
-  
-    conf: {},
-  
-    get help () {
-      return {
-        name: 'say',
-        category: 'Moderação',
-        description: 'Faz o bot enviar tal mensagem.',
-        usage: 'say',
-        admin: true
-      }
-    }
-  
-  }
+    name: "say",
+    execute,
+}
