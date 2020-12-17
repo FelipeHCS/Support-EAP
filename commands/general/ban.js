8afccf8c48968ea862a6c97ba198cc377b98193d
unknown <hilariofelipe616@gmail.com>
2020-12-16 21:38:48 -0300

    meu arquivo

--- commands/general/ban.js
+++ commands/general/ban.js
@@ -9,9 +9,9 @@ const execute = (bot, message, args) => {
   let reason = args[1];
 
   if (args.length === 0) return message.reply(`Utilize !ban <@usuario> <@motivo>`);
-  if (user.id === '780133688859951104') return message.reply("Você não pode banir o próprio bot!");
-  if (user.id === '480810583390683147') return message.reply("Você não pode banir o fundador do Servidor!");
-  const bChannel = message.guild.channels.cache.get('706552904022229065');
+  if (user.id === '780534836192215051') return message.reply("Você não pode banir o próprio bot!");
+  if (user.id === '450828668751839244') return message.reply("Você não pode banir o fundador do Servidor!");
+  const bChannel = message.guild.channels.cache.get('788927906381430784');
 
   if (!user) {
     return message.reply("Você deve informar o usuário!")
